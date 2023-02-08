47b9ae9acdb03928d1ecb8f6348ed91d111202a0
Viswajith Muraleedharan <102050284+ViswajithMuraleedharan@users.noreply.github.com>
2023-02-08 22:43:03 +0530

    Add files via upload

---
+++ User_Management_System/src/main/java/com/example/User_Management_System/UserManagementSystem.java
@@ -0,0 +1,13 @@
+package com.example.User_Management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementSystem {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementSystem.class, args);
+	}
+
+}
